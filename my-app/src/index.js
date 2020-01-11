4fcb8b53610d5109c0dad7d0618bc3f557c9381f
Tom Nguyen <tomriddle1403@gmail.com>
2020-01-10 18:15:14 -0600

    Add algorithm to check for winner

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -22,13 +22,58 @@ class Board extends React.Component {
 		};
 	}
 
+	vector(row, col) {
+		return {row: row, col: col};
+	}
+
+	checkWinner(board) {
+		const winCondition = 3; //3 'X' or 'O' in a row, col, or diagonal
+		const positions = [
+			this.vector(0, 1),//horizontal
+			this.vector(1, 0), //vertical
+			this.vector(1,1), //leftDiagonal
+			this.vector(1, -1) //rightDiagonal
+		]
+		const player = this.props.player ? 'X' : 'O';
+		for (let row = 0; row < 3; ++row) {
+			for (let col = 0; col < 3; ++col) {
+				if (board[row][col] === player) {
+					for (let pos = 0; pos < 4; ++pos) {
+						let t_row =  row;
+						let t_col = col;
+						let winCounter = 1;
+						for (let i = 1; i < winCondition; ++i) {
+							t_row += positions[pos].row;
+							t_col += positions[pos].col;
+							if (board[t_row] && board[t_row][t_col] === player)
+								winCounter += 1;
+							else
+								break;
+						}
+
+						if (winCounter === winCondition)
+						{
+							console.log("Winner is: ", player)
+							return true;
+						}
+					}
+				}
+			}
+		}
+		return false;
+	}
+
 	handleButton(rowIndex, colIndex) {
+		if (this.props.winner)
+			return ;
 		let newBoard = this.state.board.slice();
+		if (newBoard[rowIndex][colIndex])
+			return ;
 		newBoard[rowIndex][colIndex] = this.props.player ? 'X' : 'O';
-		this.props.onPlayer();
+		const hasWinner = this.checkWinner(newBoard);
+		this.props.onPlayer({hasWinner: hasWinner});
 		this.setState({
-			board: newBoard,
-			isXNext: !this.state.isXNext
+			board: newBoard
 		});
 	}
 
@@ -63,26 +108,33 @@ class Game extends React.Component {
 		super(props);
 		this.state = {
 			status: 'Next player is: X',
-			isXNext: true
+			isXNext: true,
+			hasWinner: false
 		}
 	}
 
-	handlePlayer() {
+	handlePlayer(data) {
+		let status = "";
+		if (data.hasWinner)
+			status = 'Winner is: ' + (this.state.isXNext ? 'X' : 'O');
+		else
+			status = 'Next player is: ' + (!this.state.isXNext ? 'X' : 'O');
 		this.setState({
-			isXNext: !this.state.isXNext
+			status: status,
+			isXNext: !this.state.isXNext,
+			hasWinner: data.hasWinner,
 		});
 	}
 
 	render() {
 		const length = 3;
-		const status = 'Next player is: ' + (this.state.isXNext ? 'X' : 'O');
-		console.log(this.state.isXNext);
+
 		return (
 			<>
 				<div className="player-status">
-					{status}
+					{this.state.status}
 				</div>
-				<Board length={length} player={this.state.isXNext} onPlayer={() => this.handlePlayer()} />
+				<Board length={length} player={this.state.isXNext} winner={this.state.hasWinner} onPlayer={(data) => this.handlePlayer(data)} />//Why this work?? Why do I have to use inline function like this//
 			</>
 		);
 	}
